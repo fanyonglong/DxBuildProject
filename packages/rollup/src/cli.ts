b3e8fcf9953481193c0159d7822d11c7e080fde4
fanyonglong <315144604@qq.com>
2020-04-23 22:22:37 +0800

    aa

---
+++ packages/rollup/src/cli.ts
@@ -0,0 +1,13 @@
+import yargsParser from 'yargs-parser';
+const args=yargsParser(process.argv.slice(2),{
+    alias:{
+
+    }
+});
+
+
+(async ()=>{
+    if(args._[0]==='build'){
+        
+    }
+})();
